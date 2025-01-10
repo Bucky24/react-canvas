49575b81e8d40efe7ca61e33581e829ba2be584d
Robbert Wijtman <rwijtman@slack-corp.com>
2025-01-10 11:13:48 -0800

    more broken down

--- src/Camera.js
+++ src/Camera.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { CanvasContext } from "./main";
 import { createProgramFromSources, resizeCanvasToDisplaySize, m4, v3 } from "twgl.js";
 import { degToRad, vectorMultiply } from "./utils";
+import { FColors, FVertex } from "./tempdata";
 
 export default function Camera() {
     const { context } = useContext(CanvasContext);
@@ -46,47 +47,40 @@ export default function Camera() {
     // lookup uniforms
     var matrixLocation = context.getUniformLocation(program, "u_matrix");
 
-    // Create a buffer to put positions in
-    var positionBuffer = context.createBuffer();
-    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-    context.bindBuffer(context.ARRAY_BUFFER, positionBuffer);
-    // Put geometry data into buffer
-    setGeometry(context);
-
-    // Create a buffer to put colors in
-    var colorBuffer = context.createBuffer();
-    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = colorBuffer)
-    context.bindBuffer(context.ARRAY_BUFFER, colorBuffer);
-    // Put geometry data into buffer
-    setColors(context);
-
     var cameraAngleRadians = degToRad(0);
     var fieldOfViewRadians = degToRad(60);
 
-    drawScene();
+    //////////////////////
+    // draw camera
+    /////////////////////
+    resizeCanvasToDisplaySize(context.canvas);
 
-    // Draw the scene.
-    function drawScene() {
-        resizeCanvasToDisplaySize(context.canvas);
+    // Tell WebGL how to convert from clip space to pixels
+    context.viewport(0, 0, context.canvas.width, context.canvas.height);
 
-        // Tell WebGL how to convert from clip space to pixels
-        context.viewport(0, 0, context.canvas.width, context.canvas.height);
+    // Clear the canvas AND the depth buffer.
+    context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);
 
-        // Clear the canvas AND the depth buffer.
-        context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);
+    // Turn on culling. By default backfacing triangles
+    // will be culled.
+    context.enable(context.CULL_FACE);
 
-        // Turn on culling. By default backfacing triangles
-        // will be culled.
-        context.enable(context.CULL_FACE);
+    // Enable the depth buffer
+    context.enable(context.DEPTH_TEST);
 
-        // Enable the depth buffer
-        context.enable(context.DEPTH_TEST);
+    // Tell it to use our program (pair of shaders)
+    context.useProgram(program);
 
-        // Tell it to use our program (pair of shaders)
-        context.useProgram(program);
+    // Turn on the position attribute
+    context.enableVertexAttribArray(positionLocation);
 
-        // Turn on the position attribute
-        context.enableVertexAttribArray(positionLocation);
+    const bindGeometry = (geometry) => {
+        // Create a buffer to put positions in
+        var positionBuffer = context.createBuffer();
+        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+        context.bindBuffer(context.ARRAY_BUFFER, positionBuffer);
+        // Put geometry data into buffer
+        setGeometry(context, geometry);
 
         // Bind the position buffer.
         context.bindBuffer(context.ARRAY_BUFFER, positionBuffer);
@@ -99,10 +93,18 @@ export default function Camera() {
         var offset = 0;        // start at the beginning of the buffer
         context.vertexAttribPointer(
             positionLocation, size, type, normalize, stride, offset);
+    }
+
+    const bindColors = (colors) => {
+        // Create a buffer to put colors in
+        var colorBuffer = context.createBuffer();
+        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = colorBuffer)
+        context.bindBuffer(context.ARRAY_BUFFER, colorBuffer);
+        // Put geometry data into buffer
+        setColors(context, colors);
 
         // Turn on the color attribute
         context.enableVertexAttribArray(colorLocation);
-
         // Bind the color buffer.
         context.bindBuffer(context.ARRAY_BUFFER, colorBuffer);
 
@@ -114,178 +116,61 @@ export default function Camera() {
         var offset = 0;               // start at the beginning of the buffer
         context.vertexAttribPointer(
             colorLocation, size, type, normalize, stride, offset);
+    }
 
+    var radius = 200;
 
-        var numFs = 5;
-        var radius = 200;
+    // Compute the projection matrix
+    var aspect = context.canvas.clientWidth / context.canvas.clientHeight;
+    var zNear = 1;
+    var zFar = 2000;
+    var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
 
-        // Compute the projection matrix
-        var aspect = context.canvas.clientWidth / context.canvas.clientHeight;
-        var zNear = 1;
-        var zFar = 2000;
-        var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
+    // Compute a matrix for the camera
+    var cameraMatrix = m4.rotationY(cameraAngleRadians);
+    cameraMatrix = m4.translate(cameraMatrix, v3.create(0, 0, radius * 1.5));
 
-        // Compute a matrix for the camera
-        var cameraMatrix = m4.rotationY(cameraAngleRadians);
-        cameraMatrix = m4.translate(cameraMatrix, v3.create(0, 0, radius * 1.5));
+    // Make a view matrix from the camera matrix
+    var viewMatrix = m4.inverse(cameraMatrix);
 
-        // Make a view matrix from the camera matrix
-        var viewMatrix = m4.inverse(cameraMatrix);
+    // Compute a view projection matrix
+    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
 
-        // Compute a view projection matrix
-        var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+    const drawAtPosition = (x, y, z) => {
+        // starting with the view projection matrix
+        // compute a matrix for the F
+        var matrix = m4.translate(viewProjectionMatrix, v3.create(x, z, y));
 
-        for (var ii = 0; ii < numFs; ++ii) {
-            var angle = ii * Math.PI * 2 / numFs;
-            var x = 10;//Math.cos(angle) * radius;
-            var y = 0;//Math.sin(angle) * radius;
-            var z = 10;
+        // Set the matrix.
+        context.uniformMatrix4fv(matrixLocation, false, matrix);
 
-            // starting with the view projection matrix
-            // compute a matrix for the F
-            var matrix = m4.translate(viewProjectionMatrix, v3.create(x, z, y));
+        // Draw the geometry.
+        var primitiveType = context.TRIANGLES;
+        var offset = 0;
+        var count = 16 * 6;
+        context.drawArrays(primitiveType, offset, count);
+    }
 
-            // Set the matrix.
-            context.uniformMatrix4fv(matrixLocation, false, matrix);
+    ////////////////////
+    // draw items
+    ////////////////////
 
-            // Draw the geometry.
-            var primitiveType = context.TRIANGLES;
-            var offset = 0;
-            var count = 16 * 6;
-            context.drawArrays(primitiveType, offset, count);
-        }
+    bindGeometry(FVertex);
+    bindColors(FColors);
+
+    var numFs = 5;
+
+    for (var ii = 0; ii < numFs; ++ii) {
+        var angle = ii * Math.PI * 2 / numFs;
+        var x = 10;//Math.cos(angle) * radius;
+        var y = 0;//Math.sin(angle) * radius;
+        var z = 10;
+
+        drawAtPosition(x, y, x);
     }
 
         // Fill the buffer with the values that define a letter 'F'.
-        function setGeometry(context) {
-        var positions = new Float32Array([
-                // left column front
-                0,   0,  0,
-                0, 150,  0,
-                30,   0,  0,
-                0, 150,  0,
-                30, 150,  0,
-                30,   0,  0,
-
-                // top rung front
-                30,   0,  0,
-                30,  30,  0,
-                100,   0,  0,
-                30,  30,  0,
-                100,  30,  0,
-                100,   0,  0,
-
-                // middle rung front
-                30,  60,  0,
-                30,  90,  0,
-                67,  60,  0,
-                30,  90,  0,
-                67,  90,  0,
-                67,  60,  0,
-
-                // left column back
-                    0,   0,  30,
-                30,   0,  30,
-                    0, 150,  30,
-                    0, 150,  30,
-                30,   0,  30,
-                30, 150,  30,
-
-                // top rung back
-                30,   0,  30,
-                100,   0,  30,
-                30,  30,  30,
-                30,  30,  30,
-                100,   0,  30,
-                100,  30,  30,
-
-                // middle rung back
-                30,  60,  30,
-                67,  60,  30,
-                30,  90,  30,
-                30,  90,  30,
-                67,  60,  30,
-                67,  90,  30,
-
-                // top
-                    0,   0,   0,
-                100,   0,   0,
-                100,   0,  30,
-                    0,   0,   0,
-                100,   0,  30,
-                    0,   0,  30,
-
-                // top rung right
-                100,   0,   0,
-                100,  30,   0,
-                100,  30,  30,
-                100,   0,   0,
-                100,  30,  30,
-                100,   0,  30,
-
-                // under top rung
-                30,   30,   0,
-                30,   30,  30,
-                100,  30,  30,
-                30,   30,   0,
-                100,  30,  30,
-                100,  30,   0,
-
-                // between top rung and middle
-                30,   30,   0,
-                30,   60,  30,
-                30,   30,  30,
-                30,   30,   0,
-                30,   60,   0,
-                30,   60,  30,
-
-                // top of middle rung
-                30,   60,   0,
-                67,   60,  30,
-                30,   60,  30,
-                30,   60,   0,
-                67,   60,   0,
-                67,   60,  30,
-
-                // right of middle rung
-                67,   60,   0,
-                67,   90,  30,
-                67,   60,  30,
-                67,   60,   0,
-                67,   90,   0,
-                67,   90,  30,
-
-                // bottom of middle rung.
-                30,   90,   0,
-                30,   90,  30,
-                67,   90,  30,
-                30,   90,   0,
-                67,   90,  30,
-                67,   90,   0,
-
-                // right of bottom
-                30,   90,   0,
-                30,  150,  30,
-                30,   90,  30,
-                30,   90,   0,
-                30,  150,   0,
-                30,  150,  30,
-
-                // bottom
-                0,   150,   0,
-                0,   150,  30,
-                30,  150,  30,
-                0,   150,   0,
-                30,  150,  30,
-                30,  150,   0,
-
-                // left side
-                0,   0,   0,
-                0,   0,  30,
-                0, 150,  30,
-                0,   0,   0,
-                0, 150,  30,
-                0, 150,   0]);
+    function setGeometry(context, geometry) {
 
         // Center the F around the origin and Flip it around. We do this because
         // we're in 3D now with and +Y is up where as before when we started with 2D
@@ -297,148 +182,21 @@ export default function Camera() {
         var matrix = m4.rotationX(Math.PI);
         matrix = m4.translate(matrix, v3.create(-50, -75, -15));
 
-        for (var ii = 0; ii < positions.length; ii += 3) {
-            var vector = vectorMultiply([positions[ii + 0], positions[ii + 1], positions[ii + 2], 1], matrix);
-            positions[ii + 0] = vector[0];
-            positions[ii + 1] = vector[1];
-            positions[ii + 2] = vector[2];
+        for (var ii = 0; ii < geometry.length; ii += 3) {
+            var vector = vectorMultiply([geometry[ii + 0], geometry[ii + 1], geometry[ii + 2], 1], matrix);
+            geometry[ii + 0] = vector[0];
+            geometry[ii + 1] = vector[1];
+            geometry[ii + 2] = vector[2];
         }
 
-        context.bufferData(context.ARRAY_BUFFER, positions, context.STATIC_DRAW);
+        context.bufferData(context.ARRAY_BUFFER, FVertex, context.STATIC_DRAW);
     }
 
     // Fill the buffer with colors for the 'F'.
-    function setColors(context) {
+    function setColors(context, colors) {
         context.bufferData(
             context.ARRAY_BUFFER,
-            new Uint8Array([
-                // left column front
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-
-                // top rung front
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-
-                // middle rung front
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-                200,  70, 120,
-
-                // left column back
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-
-                // top rung back
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-
-                // middle rung back
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-                80, 70, 200,
-
-                // top
-                70, 200, 210,
-                70, 200, 210,
-                70, 200, 210,
-                70, 200, 210,
-                70, 200, 210,
-                70, 200, 210,
-
-                // top rung right
-                200, 200, 70,
-                200, 200, 70,
-                200, 200, 70,
-                200, 200, 70,
-                200, 200, 70,
-                200, 200, 70,
-
-                // under top rung
-                210, 100, 70,
-                210, 100, 70,
-                210, 100, 70,
-                210, 100, 70,
-                210, 100, 70,
-                210, 100, 70,
-
-                // between top rung and middle
-                210, 160, 70,
-                210, 160, 70,
-                210, 160, 70,
-                210, 160, 70,
-                210, 160, 70,
-                210, 160, 70,
-
-                // top of middle rung
-                70, 180, 210,
-                70, 180, 210,
-                70, 180, 210,
-                70, 180, 210,
-                70, 180, 210,
-                70, 180, 210,
-
-                // right of middle rung
-                100, 70, 210,
-                100, 70, 210,
-                100, 70, 210,
-                100, 70, 210,
-                100, 70, 210,
-                100, 70, 210,
-
-                // bottom of middle rung.
-                76, 210, 100,
-                76, 210, 100,
-                76, 210, 100,
-                76, 210, 100,
-                76, 210, 100,
-                76, 210, 100,
-
-                // right of bottom
-                140, 210, 80,
-                140, 210, 80,
-                140, 210, 80,
-                140, 210, 80,
-                140, 210, 80,
-                140, 210, 80,
-
-                // bottom
-                90, 130, 110,
-                90, 130, 110,
-                90, 130, 110,
-                90, 130, 110,
-                90, 130, 110,
-                90, 130, 110,
-
-                // left side
-                160, 160, 220,
-                160, 160, 220,
-                160, 160, 220,
-                160, 160, 220,
-                160, 160, 220,
-                160, 160, 220]),
+            colors,
             context.STATIC_DRAW);
     }
 }
